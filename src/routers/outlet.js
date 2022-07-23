3f4909f9d935420059b2bc62dc4172af0a4958c2
Farhan Rivaldy <fariv.fariv12@gmail.com>
2022-07-24 01:47:16 +0800

    add calendar vue

--- src/routers/outlet.js
+++ src/routers/outlet.js
@@ -1,4 +1,5 @@
 export default [
+    { path: '/date', component: () => import('@/views/outlet/Date.vue') },
     { path: '/outlets', component: () => import('@/views/outlet/Outlet.vue') },
     { path: '/detail/outlet', component: () => import('@/views/outlet/Detail.vue') },
 
